fdfc3e24b655311728ad156357f7e7352ad7c142
therakib7 <therakib7@gmail.com>
2020-05-05 13:28:51 +0600

    added account modules

---
+++ lib/eshop/users/user_permission.ex
@@ -0,0 +1,18 @@
+defmodule Eshop.Users.UserPermission do
+  use Ecto.Schema
+  import Ecto.Changeset
+
+  schema "user_permissions" do
+    field :user_id, :id
+    field :permission_id, :id
+
+    timestamps()
+  end
+
+  @doc false
+  def changeset(user_permission, attrs) do
+    user_permission
+    |> cast(attrs, [])
+    |> validate_required([])
+  end
+end
