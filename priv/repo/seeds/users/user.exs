a58ccff7e6d2ad63ef270d6c04f73b0652ae2d60
therakib7 <therakib7@gmail.com>
2020-05-07 16:38:52 +0600

    added cominion

--- priv/repo/seeds/users/user.exs
+++ priv/repo/seeds/users/user.exs
@@ -4,7 +4,10 @@ alias Eshop.Users.User
 %User{
   first_name: "Shuvo Shaha",
   last_name: "Shuvo",
-  email: "theshuvo.01@gmail.com",
+  email: "DDtheshuvo.01@gmail.com",
   mobile: "420420420420",
-  password_hash: "98s90fw54j23908sdfiscDDxsf"
-} |> Repo.insert!
+  password: "rakib",
+  password_confirmation: "rakib",
+} 
+|> User.changeset()
+|> Repo.insert()
