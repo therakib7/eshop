cf7690e581ea01aaa169730e5a4f969ba5c1db8c
shuvo <shuvo@shopniltech.com>
2020-05-14 22:40:35 +0600

    .

--- lib/eshop_web/graphql/modules/auth/resolvers/auth_login.ex
+++ lib/eshop_web/graphql/modules/auth/resolvers/auth_login.ex
@@ -2,8 +2,9 @@ defmodule EshopWeb.Schema.Resolvers.AuthLogin do
     def login(%{email: email, password: password}, _info) do
         with {:ok, user} <- Eshop.Auth.UserManager.authenticate_user(email, password),
             {:ok, jwt, _} <- Eshop.Auth.UserManager.Guardian.encode_and_sign(user),
-            {:ok, token} <- Eshop.AuthTokens.after_encode_and_sign(nil,user,jwt,nil)  do
+            {:ok, token} <- Eshop.AuthTokens.after_encode_and_sign(user.id,%{"typ" => "access"},jwt,"fivji")  do
             {:ok, %{id: token}}
+            
         end
     end
 end
