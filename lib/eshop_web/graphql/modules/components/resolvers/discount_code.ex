b2b62c5e8fe05a163d79e3a62a25a9c6d0b19e3b
therakib7 <therakib7@gmail.com>
2020-05-14 14:03:30 +0600

    added auth

--- lib/eshop_web/graphql/modules/components/resolvers/discount_code.ex
+++ lib/eshop_web/graphql/modules/components/resolvers/discount_code.ex
@@ -1,26 +1,26 @@
 defmodule EshopWeb.Schema.Resolvers.DiscountCode do
 	 
-   	def list_iscount_codes(_parent, _args, _resolution) do
-        {:ok, Eshop.Components.list_iscount_codes()}
+   	def list_discount_codes(_parent, _args, _resolution) do
+        {:ok, Eshop.Components.list_discount_codes()}
     end
 
-    def get_iscount_code(_, %{id: id}, _) do
-		{:ok, Eshop.Components.get_iscount_code!(id)} 
+    def get_discount_code(_, %{id: id}, _) do
+		{:ok, Eshop.Components.get_discount_code!(id)} 
 	end 
 	 
-    def create_iscount_code(_parent, args, _resolution) do
-        Eshop.Components.create_iscount_code(args)
+    def create_discount_code(_parent, args, _resolution) do
+        Eshop.Components.create_discount_code(args)
     end 
 
-    def update_iscount_code(%{id: id, iscount_code_params: iscount_code_params}, _info) do
-		case {:ok, Eshop.Components.get_iscount_code!(id)} do
-			{:ok, iscount_code} -> iscount_code |> Eshop.Components.update_iscount_code(iscount_code_params) 
+    def update_discount_code(%{id: id, discount_code_params: discount_code_params}, _info) do
+		case {:ok, Eshop.Components.get_discount_code!(id)} do
+			{:ok, discount_code} -> discount_code |> Eshop.Components.update_discount_code(discount_code_params) 
 		end  
 	end
 
-	def delete_iscount_code(%{id: id}, _info) do 
-		case {:ok, Eshop.Components.get_iscount_code!(id)} do
-			{:ok, iscount_code} -> iscount_code |> Eshop.Components.delete_iscount_code() 
+	def delete_discount_code(%{id: id}, _info) do 
+		case {:ok, Eshop.Components.get_discount_code!(id)} do
+			{:ok, discount_code} -> discount_code |> Eshop.Components.delete_discount_code() 
 		end 
 	end  
 end
