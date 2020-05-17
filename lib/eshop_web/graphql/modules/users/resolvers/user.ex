f9979624bdab9e84cc2e41875b71f6851db069e8
shuvo <shuvo@shopniltech.com>
2020-05-17 14:46:19 +0600

    get role in resolver

--- lib/eshop_web/graphql/modules/users/resolvers/user.ex
+++ lib/eshop_web/graphql/modules/users/resolvers/user.ex
@@ -1,7 +1,15 @@
 defmodule EshopWeb.Schema.Resolvers.User do
   alias Eshop.Users
+  import Ecto.Query, only: [from: 2]
+
   def list_users(_parent, _args, _resolution) do
-    {:ok, Eshop.Users.list_users()}
+    # {:ok, Eshop.Users.list_users()}
+    query =
+      from u in Eshop.Users.UserRole,
+        where: u.user_id == 13,
+        select: %{id: u.id, user_id: u.user_id}
+    hello = Eshop.Repo.all(query)
+    {:ok, hello}
   end
 
   def get_user(_, %{id: id}, _resolution) do
