5d4e2d2943113c9bd1a7353f562cf6690bd753b8
therakib7 <therakib7@gmail.com>
2020-06-05 14:35:24 +0600

    remove some comment data

--- lib/eshop_web/graphql/modules/users/resolvers/user.ex
+++ lib/eshop_web/graphql/modules/users/resolvers/user.ex
@@ -1,8 +1,5 @@
 defmodule EshopWeb.Schema.Resolvers.User do
   alias EshopWeb.Graphql.Middleware.Authorize
-  alias EshopWeb.Graphql.Middleware.Permission
-  use Absinthe.Schema.Notation
-  @behaviour Absinthe.Middleware
 
   def relay_list_users(_, args, _) do
     #args = Map.put(args, :order_by, %{sort_order: :desc}) 
@@ -25,20 +22,15 @@ defmodule EshopWeb.Schema.Resolvers.User do
     Eshop.Users.create_user(args)
   end
 
-  def update_user(%{id: id, user_params: user_params}, resolution) do
-    # IO.inspect(String.to_integer())
-    # middleware(Permission.shuvo(resolution))
-    {:error, {:error, "Post id not found"}}
-    resolution
-        |> Absinthe.Resolution.put_result({:error, message: "Permission Denied", code: 303})
-
+  def update_user(%{id: id, user_params: user_params}, %{context: %{current_user: current_user}}) do
+       
     old_user = Eshop.Users.get_user!(id) 
     # IO.inspect(current_user["sub"] )
-    # if String.to_integer(current_user["sub"]) == old_user.id do
-    #   IO.inspect(old_user.id )
-    # else 
-    #   IO.inspect("tor matha" )
-    # end
+    if String.to_integer(current_user["sub"]) == old_user.id do
+      IO.inspect(old_user.id )
+    else 
+      IO.inspect("tor matha" )
+    end
 
     case {:ok, old_user} do
       {:ok, user} -> user |> Eshop.Users.update_user(user_params)
