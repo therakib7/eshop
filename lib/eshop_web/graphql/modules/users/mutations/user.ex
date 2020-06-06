353691709fb1125bfe716dd1583be50bf9ce5580
shuvo <shuvo@shopniltech.com>
2020-06-06 21:08:48 +0600

    .

--- lib/eshop_web/graphql/modules/users/mutations/user.ex
+++ lib/eshop_web/graphql/modules/users/mutations/user.ex
@@ -1,10 +1,7 @@
 defmodule EshopWeb.Schema.Mutations.User do
   use Absinthe.Schema.Notation
 
-  alias EshopWeb.Graphql.Middleware.Auth
-  alias EshopWeb.Graphql.Middleware.UserPer
   alias EshopWeb.Schema.Resolvers.User
-
   object :user_mutations do
     @desc "Create a user"
     field :create_user, type: :user do
@@ -17,15 +14,15 @@ defmodule EshopWeb.Schema.Mutations.User do
       arg(:password, :string)
       arg(:password_confirmation, :string)
       arg(:pin, :integer)
+      arg(:verified_email, :datetime)
+      arg(:verified_phone, :datetime)
+      arg(:verified_user, :datetime)
 
       resolve(&User.create_user/3)
     end
 
     @desc "Update a user"
-
-    field :update_user, type: :user do 
-      middleware(Auth) 
-      middleware(UserPer, %{p: 50, m: "user"}) 
+    field :update_user, type: :user do
       arg(:id, non_null(:id))
       arg(:user_params, :user_params)
       resolve(&User.update_user/2)
