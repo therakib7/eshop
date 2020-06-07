92147d4b27a2a5181e1d16903aa29bce133e2ab0
therakib7 <therakib7@gmail.com>
2020-06-07 16:20:38 +0600

    added company with user

--- priv/repo/seeds/users/user.exs
+++ priv/repo/seeds/users/user.exs
@@ -1,10 +1,10 @@
 alias Eshop.Users
 
 Users.create_user(%{
-  first_name: "Rakib Hasan",
-  surname: "Rakib",
-  email: "rakib@gmail.com",
+  first_name: "Hasan Hasan",
+  surname: "Hasan",
+  email: "hasan@gmail.com",
   mobile: "123456",
-  password: "rakb123456",
-  password_confirmation: "rakb123456"
+  password: "hasan123456",
+  password_confirmation: "hasan123456"
 })
